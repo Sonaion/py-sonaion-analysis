fe9b40097c0c159cd76d211da34ea0494b62daeb
Jonas Mucke <jonas.mucke@web.de>
2021-10-07 15:07:25 +0200

    setup structure for projeect

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # py-sonaion-analysis
+
+This will be a library for analysing/ visualizing REyeker, REyeker 2.0, Eyetracking, EEG and Behavioral Data.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="py-sonaion-analysis",
+    version="0.0.1",
+    description="Library for creating visualiziations and analysations of REyeker, REyeker 2.0, Eyetracking, EEG and Behavioral Data.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sonaion/py-sonaion-analysis",
+    author="Jonas Mucke",
+    author_email="jonas.mucke@web.com",
+    license="BSD 3-Clause",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["sonaion_analysis"],
+    include_package_data=True,
+    install_requires=[""],
+)
