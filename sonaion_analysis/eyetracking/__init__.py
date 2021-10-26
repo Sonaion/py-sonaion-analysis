e743336aaa08c370c7f607c9160c49bb0fb513eb
Jonas Mucke <jonas.mucke@web.de>
2021-10-26 12:27:20 +0200

    may work now?

--- sonaion_analysis/eyetracking/__init__.py
+++ sonaion_analysis/eyetracking/__init__.py
@@ -1,1 +1,7 @@
-
+from . import field_of_view
+from . import fixxationmap
+from . import heatmap
+from . import metrics
+from . import preprocessing_denoise
+from . import preprocessing_invalid
+from . import sequence
